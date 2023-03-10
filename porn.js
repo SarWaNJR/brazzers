b154d1697ed5a5cab5dc0c55b4c0d0ad21ad084e
SarWaNJR <93028248+SarWaNJR@users.noreply.github.com>
2023-03-10 16:29:31 +0530

    Add files via upload

---
+++ porn.js
@@ -0,0 +1,17 @@
+//login codes
+let a = document.getElementById('popup');
+function openPapa() {
+    a.classList.toggle('new-js')
+}
+function closeThapa() {
+    a.classList.toggle('new-js')
+}
+//join codes
+let b = document.getElementById('popuptwo');
+function Jointhis() {
+    b.classList.toggle('news-js')
+}
+function SubmitPapa() {
+    b.classList.toggle('news-js')
+}
+
